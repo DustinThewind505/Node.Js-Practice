6d132311c03da44f487aec0b3e400c010aa20d4d
unknown <Dustin.Guillen14@yahoo.com>
2020-05-17 15:53:21 -0600

    added 404 middleware andreq.name middleware

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
-
 server.use(morgan('dev'));
 server.use(helmet());
+server.use(express.json());
+server.use(logger);
 
 server.use('/api/hubs', hubsRouter);
 
-server.get('/', (req, res) => {
+// server.use(addName);
+
+server.get('/', addName, (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
 
   res.send(`
@@ -21,5 +23,25 @@ server.get('/', (req, res) => {
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
 });
+
+server.use(function(req, res){
+  res.status(404).json({message: "Oops, we could not find what you're looking for"})
+})
+
+
+
+
+function addName(req, res, next){
+  req.name = "Jason";
+  next();
+}
+
+function logger(req, res, next){
+  const method = req.method;
+  const endpoint = req.originalUrl;
+
+  console.log(`${method} to ${endpoint}`)
+  next();
+}
 
 module.exports = server;
