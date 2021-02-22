6e54ae01f809f76fd687a01461799e33b08eff8f
unknown <Dustin.Guillen14@yahoo.com>
2021-02-22 16:32:39 -0700

    FUNCTIONALITY added all 4 CRUD operations

--- 5NodeServer/index.js
+++ 5NodeServer/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const hobbits = [
+let hobbits = [
     {
         id: 1,
         name: 'Frodo',
@@ -36,13 +36,37 @@ server.get('/hobbits', (req, res) => {
     res.json(hobbits)
 })
 
+server.get('/hobbits/:id', (req, res) => {
+    const hobbit = hobbits.find(element => element.id === parseInt(req.params.id))
+
+    res.send(hobbit)
+})
+
 server.post('/hobbits', (req, res) => {
     const hobbit = req.body;
     hobbit.id = nextID++;
-  
+
     hobbits.push(hobbit);
-  
+
     res.status(201).json(hobbits);
-  });
+});
+
+server.put('/hobbits/:id', (req, res) => {
+    const hobbit = hobbits.find(hobbit => hobbit.id === parseInt(req.params.id))
+
+    if(!hobbit){
+        res.status(404).json({message: 'Hobbit not found'})
+    } else {
+        Object.assign(hobbit, req.body);
+
+        res.status(200).json(hobbit)
+    }
+})
+
+server.delete('/hobbits/:id', (req, res) => {
+    hobbits = hobbits.filter(hobbit => hobbit.id !== parseInt(req.params.id))
+
+    res.send(hobbits)
+})
 
 server.listen(8000, () => console.log(`\n\n\t*** Server is listening on port 8000 ***\n`))
