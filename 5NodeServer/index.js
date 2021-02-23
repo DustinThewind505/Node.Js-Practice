d08c7f5d79be8549e0a60eb23519bde30c7ece99
unknown <Dustin.Guillen14@yahoo.com>
2021-02-23 14:36:28 -0700

    FUNCTIONALITY added 404 page

--- 5NodeServer/index.js
+++ 5NodeServer/index.js
@@ -4,6 +4,7 @@ const hobbitsRoutes = require('./hobbits/hobbitsRoutes');
 const vampiresRouter = require('./vampires/vampiresRoute');
 
 const server = express();
+server.use(express.json());
 
 server.use('/hobbits', hobbitsRoutes);
 server.use('/vampires', vampiresRouter);
@@ -11,5 +12,10 @@ server.use('/vampires', vampiresRouter);
 server.get('/', (req, res) => {
     res.send('<h1>Got to localhost:8000/hobbits</h1>')
 });
+
+server.use(function(req, res) {
+    res.status(404).send('<h1>🛑404 Page not found🛑</h1>')
+});
+
 
 server.listen(8000, () => console.log(`\n\n\t*** Server is listening on port 8000 ***\n`));
