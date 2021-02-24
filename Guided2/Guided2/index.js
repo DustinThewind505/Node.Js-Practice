01f0db6c29a8f10c7b037ff377348933c4ddd310
unknown <Dustin.Guillen14@yahoo.com>
2021-02-24 14:26:45 -0700

    FUNCTIONALITY added middleware

--- Guided2/Guided2/index.js
+++ Guided2/Guided2/index.js
@@ -1,11 +1,26 @@
+// ========== IMPORTS ==========
 const express = require('express');
 
+const morgan = require('morgan');
 
 const hubsRoutes = require('./hubs/hubsRouter');
 
+const gatekeeper = (req, res, next) => {
+  const seconds = new Date().getSeconds();
+  console.log(seconds)
+  
+  if(seconds % 3 === 0){
+    res.status(401).json({message: 'You shall not pass'})
+  } else {
+    next()
+  }
+}
+
+// ========== SERVER ==========
 const server = express();
 server.use(express.json());
-
+server.use(morgan('tiny'));
+server.use(gatekeeper);
 server.use('/api/hubs', hubsRoutes);
 
 server.get('/', (req, res) => {
