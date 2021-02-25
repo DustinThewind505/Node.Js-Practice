a5404bade666b7127ac3ce6041245192eb666117
unknown <Dustin.Guillen14@yahoo.com>
2021-02-25 08:47:18 -0700

    FUNCTIONALITY and gitignore, dotenv dependency, and .env file

--- 3ExpressServer/index.js
+++ 3ExpressServer/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const server = require('./server');
 
+const port = process.env.PORT
 
-
-server.listen(8000, () => console.log(`\n\n\t*** Server listening on port 8000 ***\n`))
+server.listen(port, () => console.log(`\n\n\t*** Server listening on port ${port} ***\n`))
