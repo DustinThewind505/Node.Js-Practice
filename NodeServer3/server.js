226d8feca7bfc2345d067051e9a36d5264a830a3
unknown <Dustin.Guillen14@yahoo.com>
2023-11-21 10:45:53 -0700

    [ticket number] imported JSON data, added url for each product, added homepage, added styling to 404 page

--- NodeServer3/server.js
+++ NodeServer3/server.js
@@ -1,12 +1,38 @@
 // ===== IMPORTS =====
 const http = require("http");
+const Products = require("./Data/sampleData.json");
 
 // ===== VARIABLES =====
 const PORT = 8000;
 
 // ===== SERVER =====
 const server = http.createServer((req, res) => {
-    if(req.url === "/hello") {
+    if(req.url === "/products") {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "text/html");
+        res.write("<body style='text-align:center;'><h1>Products</h1></body>");
+        res.end(JSON.stringify(Products));
+    } else if(req.url === "/products/1") {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "text/html");
+        res.write("<body style='text-align:center;'><h1>Products</h1></body>");
+        res.end(JSON.stringify(Products[0]));
+    } else if(req.url === "/products/2") {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "text/html");
+        res.write("<body style='text-align:center;'><h1>Products</h1></body>");
+        res.end(JSON.stringify(Products[1]));
+    } else if(req.url === "/products/3") {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "text/html");
+        res.write("<body style='text-align:center;'><h1>Products</h1></body>");
+        res.end(JSON.stringify(Products[2]));
+    } else if(req.url === "/products/4") {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "text/html");
+        res.write("<body style='text-align:center;'><h1>Products</h1></body>");
+        res.end(JSON.stringify(Products[3]));
+    } else if(req.url === "/hello") {
         res.statusCode = 200;
         res.setHeader("Content-type", "text/html");
         res.write("<body style='text-align:center;'><h1>Konnichiwa</h1></body>");
@@ -16,15 +42,20 @@ const server = http.createServer((req, res) => {
         res.setHeader("Content-type", "text/html");
         res.write("<body style='text-align:center;'><h1>One Two Three</h1></body>");
         res.end("123");
+    } else if(req.url === "/") {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "text/html");
+        res.write("<body style='text-align:center;'><h1>I want my tears back</h1></body>");
+        res.end("I want my tears back now~!");
     } else {
         res.statusCode = 404;
         res.setHeader("Content-type", "text/html");
-        res.write("<body style='background:black;color:red;text-align:center;'><h1>404 Page not found</h1></body>");
+        res.write("<body style='background:black;color:red;text-align:center;'><h1>404 Page not found</h1><a href='http://localhost:8000/'>home</a><br/><a href='http://localhost:8000/hello'>hello</a><br/><a href='http://localhost:8000/123'>123</a><br/></body>");
         res.end("Please try again");
     }
 
 })
 
 server.listen(PORT, () => {
-    console.log(`\n\n\t\t***** server listening on port 8000 *****\n`);
+    console.log(`\n\n\t\t***** server listening on port ${8000} *****\n`);
 });
