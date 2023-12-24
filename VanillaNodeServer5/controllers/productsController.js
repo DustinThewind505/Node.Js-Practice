ac090bc818b80713e40b09affe85f541e373dc1f
unknown <Dustin.Guillen14@yahoo.com>
2023-12-23 22:05:45 -0700

    [ticket number] added styling through the data object sent in the response

--- VanillaNodeServer5/controllers/productsController.js
+++ VanillaNodeServer5/controllers/productsController.js
@@ -8,12 +8,12 @@ const productsModel = require("../models/productsModels");
 
 function getAllProducts(req, res) {
 
-    const allProducts = productsModel.findAll();
+    const allProducts = JSON.stringify(productsModel.findAll());
 
     res.statusCode = 200;
     res.setHeader("Content-type", "text/html");
-    res.write(`<body style='text-align:center;'><h1>Products</h1><p>${JSON.stringify(allProducts)}</p></body>`);
-    res.end(JSON.stringify(allProducts));
+    res.write(`<body style='text-align:center;'><h1>Products</h1><p>${allProducts}</p></body>`);
+    res.end(allProducts);
 
 }
 
@@ -30,7 +30,7 @@ function getProductById(req, res, id) {
     } else {
         res.statusCode = 200;
         res.setHeader("Content-type", "text/html");
-        res.write(`<body style="background-color:darkolivegreen;color:red;text-align:center;"><h1>Product ${JSON.stringify(parseInt((product.id)))}</h1><p>${JSON.stringify(parseInt((product.id))) == 1 ? `<a></a>` : `<a style="color:cornflowerblue;" href="http://localhost:8000/products/${JSON.stringify(parseInt((product.id)) - 1)}"><< previous</a>`} ${JSON.stringify(parseInt((product.id))) == allProductslength ? `<a></a>` : `<a style="color:cornflowerblue;" href="http://localhost:8000/products/${JSON.stringify(parseInt((product.id)) + 1)}">next >></a>`}</p><h2>${JSON.stringify(product.name)}</h2><p>${JSON.stringify(product.description)}</p><p>Price: $${JSON.stringify(product.price)}</p><img style="border: 2px solid black;" src=${JSON.stringify(product.image)}/><br/></body>`)
+        res.write(`<body style="background-color:${JSON.stringify(product.styles.backgroundColor).slice(1, JSON.stringify(product.styles.backgroundColor).length - 1)};color:${JSON.stringify(product.styles.color).slice(1, JSON.stringify(product.styles.color).length - 1)};text-align:center;"><h1>Product ${JSON.stringify(parseInt((product.id)))}</h1><p>${JSON.stringify(parseInt((product.id))) == 1 ? `<a></a>` : `<a style="color:cornflowerblue;" href="http://localhost:8000/products/${JSON.stringify(parseInt((product.id)) - 1)}"><< previous</a>`} ${JSON.stringify(parseInt((product.id))) == allProductslength ? `<a></a>` : `<a style="color:cornflowerblue;" href="http://localhost:8000/products/${JSON.stringify(parseInt((product.id)) + 1)}">next >></a>`}</p><h2>${JSON.stringify(product.name)}</h2><p>${JSON.stringify(product.description)}</p><p>Price: $${JSON.stringify(product.price)}</p><img style="border: 2px solid black;" src=${JSON.stringify(product.image)}/><br/></body>`)
         res.end()
     }
 
