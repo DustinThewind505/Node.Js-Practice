066d46763892cee17b7742fd6ebc9906abd46439
unknown <Dustin.Guillen14@yahoo.com>
2024-01-02 20:31:22 -0700

    [ticket number] added cors

--- VanillaNodeServer5/server.js
+++ VanillaNodeServer5/server.js
@@ -7,6 +7,18 @@ const PORT = 8000;
 
 // ===== SERVER =====
 const server = http.createServer((req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    if ( req.method === 'OPTIONS' ) {
+        res.writeHead(200);
+        res.end();
+        return;
+    }
+
+
     if(req.url === "/products" && req.method === "GET") {
 
         getAllProducts(req, res);
